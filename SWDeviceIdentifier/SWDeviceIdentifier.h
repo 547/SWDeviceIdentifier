f8c1e79f292e77606b50516b4500435a55d610e2
Seven Wang <seven.wang@supernovachina.com>
2019-06-24 11:35:41 +0800

    Initial Commit

---
+++ SWDeviceIdentifier/SWDeviceIdentifier.h
@@ -0,0 +1,19 @@
+//
+//  SWDeviceIdentifier.h
+//  SWDeviceIdentifier
+//
+//  Created by Supernova SanDick SSD on 2019/6/24.
+//  Copyright © 2019 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWDeviceIdentifier.
+FOUNDATION_EXPORT double SWDeviceIdentifierVersionNumber;
+
+//! Project version string for SWDeviceIdentifier.
+FOUNDATION_EXPORT const unsigned char SWDeviceIdentifierVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWDeviceIdentifier/PublicHeader.h>
+
+
